5bfb12d8b86633696d2a014840928e9ec57d3006
Nandini Gattani <nandini.gattani09@gmail.com>
2025-02-15 20:35:03 +0530

    Basic project setup

---
+++ IssueTracker/settings.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+from zango.config.settings.base import *  # noqa: F403
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class AttrDict(dict):
+    """
+    A dictionary subclass for managing global settings with attribute-style access.
+
+    This class allows getting and setting items in the global namespace
+    using both attribute and item notation.
+    """
+
+    def __getattr__(self, item):
+        return globals()[item]
+
+    def __setattr__(self, item, value):
+        globals()[item] = value
+
+    def __setitem__(self, key, value):
+        globals()[key] = value
+
+
+# Call setup_settings to initialize the settings
+settings_result = setup_settings(AttrDict(vars()), BASE_DIR)
+
+# Setting Overrides
+# Any settings that need to be overridden or added should be done below this line
+# to ensure they take effect after the initial setup
+
+SECRET_KEY = "django-insecure-o65=p*tog_fiiii$ovb=b04y&l3@#0os@#m!%hlef5ws&^^(tt"  # Shift this to .env
+
+
+# To change the media storage to S3 you can use the BACKEND class provided by the default storage
+# To change the static storage to S3 you can use the BACKEND class provided by the staticfiles storage
+# STORAGES = {
+#     "default": {"BACKEND": "zango.core.storage_utils.S3MediaStorage"},
+#     "staticfiles": {"BACKEND": "zango.core.storage_utils.S3StaticStorage"},
+# }
+
+
+# INTERNAL_IPS can contain a list of IP addresses or CIDR blocks that are considered internal.
+# Both individual IP addresses and CIDR notation (e.g., '192.168.1.1' or '192.168.1.0/24') can be provided.
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+
+ALLOWED_HOSTS = ["hello.zango.com", "localhost", "127.0.0.1"]
